59fa8404a3dc90eb83e456f81e2ee69d47f0354c
Ondrej Maksi <ondrej.maksi@gmail.com>
2015-06-07 22:25:10 +0200

    added example 0.0.1

---
+++ example/gulpfile.js
@@ -0,0 +1,25 @@
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var reactify = require('reactify');
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './index.jsx',
+    debug: true,
+    // defining transforms here will avoid crashing your stream
+    transform: [reactify]
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .on('error', console.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
+});
---
+++ example/index.jsx
@@ -0,0 +1,128 @@
+var React = require('react');
+
+var Container = require('../container.jsx');
+var Row = require('../row.jsx');
+var Column = require('../column.jsx');
+
+var data = {
+  width: [],
+  pull: [],
+  push: [],
+  offset: [],
+};
+
+for (var i=1;i<13;i++){
+  data.width.push({
+    width: i,
+    text: 'width={' + i + '}',
+  });
+  data.pull.push({
+    pull: i,
+    text: 'pull={' + i + '}',
+  });
+  data.push.push({
+    push: i,
+    text: 'push={' + i + '}',
+  });
+  data.offset.push({
+    offset: i,
+    text: 'offset={' + i + '}',
+  });
+}
+
+var App = React.createClass ({
+  render: function () {
+    var s = {
+      backgroundColor: '#f3f3f3',
+      border: '1px solid #e3e3e3',
+      paddingTop: '10px',
+      paddingBottom: '10px',
+    };
+    return (
+        <div>
+          <Container>
+            <Row>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+              <Column style={s} width={1}>width={1}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={8}>width={8}</Column>
+              <Column style={s} width={4}>width={4}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={4}>width={4}</Column>
+              <Column style={s} width={4}>width={4}</Column>
+              <Column style={s} width={4}>width={4}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={6}>width={6}</Column>
+              <Column style={s} width={6}>width={6}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={9}>width={9}</Column>
+              <Column style={s} width={4}>width={4}</Column>
+              <Column style={s} width={6}>width={6}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={4}>width={4}</Column>
+              <Column style={s} width={4} offset={4}>width={4} offset={4}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={3} offset={3}>width={3} offset={3}</Column>
+              <Column style={s} width={3} offset={3}>width={3} offset={3}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={6} offset={3}>width={6} offset={3}</Column>
+            </Row>
+
+            <Row>
+              <Column style={s} width={9}>
+                width={9}
+                <Row>
+                  <Column style={s} width={6}>width={6}</Column>
+                  <Column style={s} width={6}>width={6}</Column>
+                </Row>
+              </Column>
+            </Row>
+          </Container>
+        </div>
+    );
+  }
+});
+
+var SampleElement = React.createClass ({
+  render: function () {
+    var style = {
+      width: '100%',
+      // height: 30,
+      // lineHeight: '30px',
+      // margin: '3px 0px',
+      backgroundColor: '#f3f3f3',
+      border: '1px solid #e3e3e3',
+    };
+    return (
+        <div style={style}>
+          {this.props.children}
+        </div>
+    );
+  }
+});
+
+React.render(<App/>, document.getElementById("content"));
